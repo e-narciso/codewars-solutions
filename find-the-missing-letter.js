f2bc01db6fa549db0de6b096af5093086c85878c
Enrique Narciso <46304338+e-narciso@users.noreply.github.com>
2019-09-24 09:46:41 -0400

    find-the-missing-letter

---
+++ find-the-missing-letter.js
@@ -0,0 +1,11 @@
+const findMissingLetter = array => {
+    let string = array.join("");
+    for(let i = 0; i < string.length; i++){
+        if(string.charCodeAt(i + 1) - string.charCodeAt(i) != 1){
+            return String.fromCharCode(string.charCodeAt(i) + 1);
+        }
+    }
+}
+
+console.log(findMissingLetter(['a','b','c','d','f']));
+console.log(findMissingLetter(['O','Q','R','S']));
--- persistent-bugger.js
+++ persistent-bugger.js
@@ -2,12 +2,12 @@ const persistence = num => {
   let count = 0;
   while (num > 9) {
     count++;
-    num = [...(num + "")].map(n => +n).reduce((a, b) => a * b);
+    num = [...(num + "")].map(Number).reduce((a, b) => a * b);
   }
   return count;
 };
 
 console.log(persistence(39));
-// console.log(persistence(4) === 0);
-// console.log(persistence(25) === 2);
-// console.log(persistence(999) === 4);
+console.log(persistence(4));
+console.log(persistence(25));
+console.log(persistence(999));
